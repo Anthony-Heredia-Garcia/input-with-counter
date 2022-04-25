98bfe0266e2d4e144c2cf9ea8052ccd3447598c1
Anthony-Heredia-Garcia <99502244+Anthony-Heredia-Garcia@users.noreply.github.com>
2022-04-25 19:14:39 -0400

    still working on random

--- conditional-code.js
+++ conditional-code.js
@@ -1,7 +1,5 @@
 const myName = document.getElementById("product-name");
-const submitButton = document.querySelector('button');
-console.log('empty string');
-
+const submitButton = document.querySelector("button");
 
 // if (myName === "Tony") {
 //   console.log("Hello!");
@@ -11,12 +9,13 @@ console.log('empty string');
 //   console.log("Empty String!");
 // }
 
-function myFunction() {
-    if (myName.value) {
-        console.log(myName.value);
-    } else {
-        console.log('empty string');
-    }
+// function getRandom() {
+//     randomNumber = Math.random()
+//     console.log(randomNumber);
+// }
+
+function getRandom() {
+    
 }
 
-submitButton.addEventListener('click', myFunction);
+submitButton.addEventListener("click", getRandom);
